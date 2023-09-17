f70f92ea4bf10155be9b7dd7a278b31d5630f9d8
yiungyiung <yashparkhi.super@gmail.com>
2023-09-18 00:57:30 +0530

    vg

--- Assets/Car/PlayerLapTracker.cs
+++ Assets/Car/PlayerLapTracker.cs
@@ -9,39 +9,59 @@ public class PlayerLapTracker : NetworkBehaviour
     public TMP_Text LapCounter;
     [SerializeField]
     public TMP_Text LapTimer;
+    public TMP_Text Position;
     int lapcounter = 0;
     [SerializeField]
     public GameManager gameManager;
-
+    public GameObject main;
+    public int why;
+    
+    bool dispval=false;
     void Start()
     {   
         if(!isLocalPlayer){return;}
         gameManager= GameObject.FindWithTag("GameController").GetComponent<GameManager>();
         LapCounter=GameObject.Find("Canvas/LapCounter").GetComponent<TextMeshProUGUI>();
-        LapTimer=GameObject.Find("Canvas/LapTimer").GetComponent<TextMeshProUGUI>();;    
+        LapTimer=GameObject.Find("Canvas/LapTimer").GetComponent<TextMeshProUGUI>();
+        Position=GameObject.Find("Canvas/Position").GetComponent<TextMeshProUGUI>(); 
         LapCounter.text="Lap:1/2";
+        LapTimer.text="Lap 1: ?";
             }
 
     void Update()
     {   
         if(!isLocalPlayer){return;}
+
+        if(dispval){
+            why=gameManager.GetLap();
+            Position.text=why.ToString();
+        }
         if ( halfLapTriggerPassed && fullLapTriggerPassed )
         {
             if(lapcounter==0)
             {
                 LapCounter.text="Lap:2/2";
-                LapTimer.text="Lap 1:"+gameManager.GetTime()+" s";
+                LapTimer.text="Lap 1:"+gameManager.GetTime().ToString("0.00")+" s";
             }
              else if(lapcounter==1)
             {
                 LapCounter.text="Finished";
-                LapTimer.text="Lap 2:"+gameManager.GetTime()+" s";
+                LapTimer.text="Lap 2:"+gameManager.GetTime().ToString("0.00")+" s";
             }
             halfLapTriggerPassed=false;
             fullLapTriggerPassed=false;
             lapcounter++;
-            
-        }
-    }
+                if(lapcounter>1)
+                {   
+                    gameManager.Cmdposupdate();
+                    dispval=true;
+                }
 
+
+                
+            }
+
+        }
+
+    
 }  
--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -6,6 +6,13 @@ public class GameManager : NetworkBehaviour
 {
     [SyncVar]
     public float timer;
+    public TMP_Text Position;
+    public int dispPosition=0;
+    [SyncVar]
+    public int finishPosition = 0;
+
+private bool hasUpdated = false;
+    
 
     public TMP_Text Text; // Reference to your TextMeshProUGUI component
 
@@ -23,11 +30,20 @@ public class GameManager : NetworkBehaviour
         {
             timer += Time.deltaTime;
             RpcUpdateTimer(timer);
-            VehiclePhysics[] allCars = FindObjectsOfType<VehiclePhysics>();
-            Debug.Log("Number of cars in the scene: " + allCars.Length);
         }
     }
 
+    [Command(requiresAuthority = false)]
+    public void Cmdposupdate(NetworkConnectionToClient sender = null)
+    {
+        finishPosition=finishPosition+1;
+        Rpcupdatepos(finishPosition);
+    }
+    
+    [ClientRpc]
+    public void Rpcupdatepos(int pos){
+        finishPosition=pos;
+    }
     [ClientRpc]
     void RpcUpdateTimer(float newTime)
     {
@@ -35,6 +51,10 @@ public class GameManager : NetworkBehaviour
         Text.text = timer.ToString("0.00"); // Update the TextMeshProUGUI text
     }
 
+    public int GetLap()
+    {
+        return dispPosition;
+    }
     public float GetTime()
     {
         return timer;
