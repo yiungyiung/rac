5f7f66c6fa53bcada2fc75b58c3d654e801234b1
yiungyiung <yashparkhi.super@gmail.com>
2024-07-30 01:51:50 +0530

    lol

--- Assets/Scenes/scripts/STARTSCENE.cs
+++ Assets/Scenes/scripts/STARTSCENE.cs
@@ -7,4 +7,20 @@ public class STARTSCENE : MonoBehaviour
   public void rescene(){
     SceneManager.LoadScene(SceneManager.GetActiveScene().name);
   }
+
+  public void first(){
+SceneManager.LoadScene("1stfloor");
+  }
+
+  public void sixth(){
+SceneManager.LoadScene("6thFloor");
+  }
+
+  public void third(){SceneManager.LoadScene("3rd Floor");}
+
+  public void fourth(){SceneManager.LoadScene("4thFloor");}
+
+  public void fifth(){SceneManager.LoadScene("5thFloor");}
+
+
 }
